4b39ac86730fdf5b1e5770141d343844afa1f29b
Ramanathan Nachiappan <nramssn@gmail.com>
2026-02-11 21:28:49 -0700

    [chore] - disabling the default astro island feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar: {
+        enabled: false,
+    },
     site: 'https://nramanath.github.io',
     base: '/profile',
     integrations: [
